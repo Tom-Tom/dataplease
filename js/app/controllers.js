ac6fe5b19c49f632c55b4e7f6221179734f3df35
Vincent Garreau <VincentGarreau@Vincent-Garreau.local>
2013-11-21 22:43:21 +0100

    search form

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -4,6 +4,20 @@ var datapleaseControllers = angular.module('datapleaseControllers', []);
 
 /* Controllers */
 
+datapleaseApp.controller('SearchCtrl', ['$scope', '$routeParams', 'Data', '$location', function ($scope, $routeParams, Data, $location){
+
+	$scope.submit = function (){
+
+		var	what = this.query.what,
+			where = this.query.where,
+			when = this.query.when;
+		
+		$location.path('/city/'+where).hash(what);
+	}
+
+}])
+
+
 datapleaseApp.controller('RegionsAllCtrl', ['$scope', '$routeParams', 'Data', function ($scope, $routeParams, Data){
 	
 	$scope.data = Data.getAllRegions();
@@ -16,14 +30,52 @@ datapleaseApp.controller('RegionCtrl', ['$scope', '$routeParams', 'Data', functi
 
 }])
 
+
 datapleaseApp.controller('DepartmentsAllCtrl', ['$scope', '$routeParams', 'Data', function ($scope, $routeParams, Data){
 	
-	$scope.data = Data.getAllDepartments({ regionSlug:$routeParams.regionSlug });
+	$scope.data = Data.getAllDepartments();
 
 }])
 
 datapleaseApp.controller('DepartmentCtrl', ['$scope', '$routeParams', 'Data', function ($scope, $routeParams, Data){
 	
-	$scope.data = Data.getDepartment({ regionSlug:$routeParams.regionSlug, departmentSlug:$routeParams.departmentSlug });	
+	$scope.data = Data.getDepartment({ departmentSlug:$routeParams.departmentSlug });	
 
 }])
+
+
+datapleaseApp.controller('CitiesAllCtrl', ['$scope', '$routeParams', 'Data', function ($scope, $routeParams, Data){
+	
+	$scope.data = Data.getAllCities();
+
+}])
+
+datapleaseApp.controller('CityCtrl', ['$scope', '$routeParams', 'Data', '$location', function ($scope, $routeParams, Data, $location){
+	
+	var params = { 
+		citySlug:$routeParams.citySlug
+	};
+
+	if($location.hash()){
+		params['dataId'] = $location.hash();
+	}
+
+	$scope.data = Data.getCity(params);
+
+	if(params['dataId']){
+		Data.getCity(params, function(output){
+			$scope.data = output.chomage[params['dataId']];
+		});
+	}
+
+	//console.log($location.search(), $location.hash());
+
+
+
+}])
+
+
+
+
+
+
